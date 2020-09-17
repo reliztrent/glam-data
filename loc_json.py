38f318e5db915dd4bc3974ab8f4f3868d9d98b10
reliztrent <rtrent@loc.gov>
2020-09-17 19:38:06 -0400

    remove redundant at=facet

--- loc_json.py
+++ loc_json.py
@@ -62,7 +62,7 @@ def main(argv):
             directory = arg
     print ('Start date is ', start_date)
     print ('End date is ', end_date)
-    url = 'https://www.loc.gov/search/?at=facets&fo=json&sb=shelf-id&sq=group:gmd.mar+AND+number_source_modified:[' + start_date + ' TO ' + end_date + ']'
+    url = 'https://www.loc.gov/search/?fo=json&sb=shelf-id&sq=group:gmd.mar+AND+number_source_modified:[' + start_date + ' TO ' + end_date + ']'
     print("Query URL is: ", url)
     items = query (url,[])
 
