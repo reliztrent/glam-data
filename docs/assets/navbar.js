921c62ce626e1804f3aa2e629ab5d45ac14d610f
reliztrent <rtrent@loc.gov>
2020-10-27 18:08:53 -0400

    old css edit, testing javascript for modal

--- docs/assets/navbar.js
+++ docs/assets/navbar.js
@@ -8,3 +8,23 @@ $('.sidebar .nav-link').click(function(){
         anchor_id.removeClass('glow');
     }, 3000); /This should match the number of seconds on the glow css class/
 })
+
+/* zoom modals
+*/
+var emptyModal = $('<div id="zoomModal" class="modal"></div>');
+$('main').append(emptyModal);
+
+$('.zoom').click(function(){
+	var modalCard = $('#zoomModal');
+	var card = $(this).closest(".card");
+	card.clone().appendTo( '#zoomModal');
+	modal.style.display = "block";
+})
+
+window.onclick = function(event) {
+  var modalCard = $('#zoomModal');
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+  $(#zoomModal).empty();
+}
